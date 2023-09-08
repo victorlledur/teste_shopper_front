3d536088c8ef8366d156e09b02ebafbf321f39f4
Victor Ledur <victorlledur@gmail.com>
2023-09-08 05:56:06 -0300

    fix: api communications

--- src/services/mainApi/config/index.tsx
+++ src/services/mainApi/config/index.tsx
@@ -3,6 +3,7 @@ import axios from "axios";
 const baseApi = axios.create({
   baseURL: process.env.REACT_APP_URL_BASE,
   headers: {
+    'Accept': 'application/json',
     "Content-Type": "application/json",
   },
 });
--- src/services/mainApi/products.tsx
+++ src/services/mainApi/products.tsx
@@ -8,8 +8,9 @@ export function byIdProduct(props: any) {
   return baseApi.get(`/product/${props}`);
 }
 
-export function updateProduct(req: any, props: any) {
-  return baseApi.put(`/user/${props}`, {
-    sales_price: req.sales_price,
+export async function updateProduct(req: any, props: any) {
+  const response = await baseApi.put(`/product/${props}`, {
+    sales_price: req.sales_price,    
   });
+  return response.data;
 }
